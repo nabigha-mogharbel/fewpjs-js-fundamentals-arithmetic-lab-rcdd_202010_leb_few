5d5ef0ab1f024893bc0e613b7c00ea8d0fa76796
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-11-24 10:57:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let multiply=num1*num2;
-multiply=64;
+function multiply(num1,num2){num1,num2)
+multiply(8,8);
 let random=Math.floor(Math.random()*10)+1;
 let mod=24%4;
 let max=Math.max(3,20,16,19);
